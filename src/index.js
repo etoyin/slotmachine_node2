956faeb3b60655e915c7f455954c5d5669e57eb7
kreativemind08 <kreativemind08@gmail.com>
2025-01-09 12:39:54 +0100

    swup

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
-import barba from '@barba/core';
-import gsap from 'gsap';
+import Swup from 'swup';
+
+// import barba from '@barba/core';
+// import gsap from 'gsap';
 // import barbaCss from '@barba/css';
 
 // barba.use(barbaCss);
@@ -14,64 +16,70 @@ const validateEmail = (email) => {
     );
 };
 
+async function loadScript(url){
+    return new Promise( (resolve) => {
+        const script = document.createElement('script')
+         script.type = 'text/javascript'
+          script.src = url
+          script.onload = () => resolve();
+          
+        //  document.body.appendChild(script);
+         document.getElementsByTagName('head')[0].appendChild(script);
+    })
+}
+
 document.addEventListener('DOMContentLoaded', function () {
 
-    const container = document.querySelector('.barba-container');
-    if(container){
-        console.log("container element is:", container);
-    } else{
-        console.log("barba container element not found")
-    }
-
-    const animationEnter = (container) => {
-        return gsap.from(container, { autoAlpha: 0, duration: 2, clearProps: 'all', ease: 'none'})
-    };
-
-    const animationLeave = (container, done) => {
-        return gsap.to(container, { autoAlpha: 0, duration: 4, clearProps: 'all', ease: 'none', onComplete: () => done()})
-    }
-
-
-
-    barba.init({
-        transitions: [{
-            name: 'default-transition',
-            // async once({next}){
-            //     animationEnter(next.container)
-            // },
-
-            // async leave({current}){
-            //     console.log('Leaving...');
-            //     const done = this.async();
-                
-            //     animationLeave(current.container, done);
-            // },
-
-            // async enter({next}){
-            //     console.log('Entering...');
-            //     // const done = this.async();
-            //     animationEnter(next.container);
-            // }
-            async leave(data){
-                const done = this.async()
-                await gsap.to(data.current.container, {
-                    opacity: 0,
-                    duration: 0.5,
-                    onComplete: done
-                })
-
-            },
-            async enter(data){
-                const done = this.async()
-                await gsap.from(data.next.container, {
-                    opacity: 0,
-                    duration: 0.5,
-                    onComplete: done
-                })
-            },
-        }]
+    const swup = new Swup({
+        containers: ['#swup'],
+        animateHistoryBrowsing: true,
+        // transitions: [/* Define transitions */],
     });
+    // swup.hooks.on('visit:start', () => {
+    //     console.log(window.location.href);
+    // })
 
+    
+
+
+    // barba.init({
+    //     transitions: [{
+    //         name: 'default-transition',
+    //         async leave(data){
+    //             const done = this.async()
+    //             await gsap.to(data.current.container, {
+    //                 opacity: 0,
+    //                 duration: 0.5,
+    //                 onComplete: done
+    //             })
+
+    //         },
+    //         async enter(data){
+    //             const done = this.async()
+    //             await gsap.from(data.next.container, {
+    //                 opacity: 0,
+    //                 duration: 0.5,
+    //                 onComplete: done
+    //             })
+    //         },
+    //     }]
+    // });
+
+    swup.hooks.on('content:replace', async (event) => {
+        // Get the new document
+        // console.log("content replaced:", event.to.html);
+        const newDoc = new DOMParser().parseFromString(event.to.html, 'text/html');
+        const scripts = newDoc.querySelectorAll('script[type="text/javascript"]');
+        for(const script of scripts) {
+            console.log(script);
+            
+            if (script.src) {
+                await loadScript(script.src);
+            }
+        };
+        console.log(document.body);
+
+    });
     // console.log("klkl");
     const submit = document.getElementById('submit');
     // document.getElementById('submit').textContent = "Loading...";
@@ -97,10 +105,10 @@ document.addEventListener('DOMContentLoaded', function () {
                     // window.location.replace("/home");
                     if(window.innerWidth > 600){
                         console.log("Window Width:",window.innerWidth);
-                        barba.go('/home');
+                        swup.navigate('/home');
                     }
                     else{
-                        barba.go('/mobile');
+                        swup.navigate('/mobile');
                     }
                 }
                 else{
@@ -131,10 +139,10 @@ document.addEventListener('DOMContentLoaded', function () {
     back?.addEventListener("click", async function() {
         console.log("Window Width:",window.innerWidth);
         if(window.innerWidth > 600){
-            barba.go('/home');
+            swup.navigate('/home');
         }
         else{
-            barba.go('/mobile');
+            swup.navigate('/mobile');
         }
     });
 
